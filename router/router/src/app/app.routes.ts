bf5325a8892afd6ed0f42dc7fb2e58537a5c2ef5
teddy <hochart.dev@gmail.com>
2022-04-06 11:42:47 +0200

    router quest

--- router/router/src/app/app.routes.ts
+++ router/router/src/app/app.routes.ts
@@ -1,6 +1,4 @@
-import { Component } from "@angular/core";
 import { Routes } from "@angular/router";
-import { AppComponent } from "./app.component";
 import { SignUpComponent } from "./menu/sign-up/sign-up.component";
 import { UserProfileComponent } from "./menu/user-profile/user-profile.component";
 
